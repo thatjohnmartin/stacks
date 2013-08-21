6cf4481922693dfcac01daf1bfc3c7373ee14ae3
John Martin <john@lonepixel.com>
2013-08-20 22:11:39 -0700

    Added a library item model.

--- stacks/www/models/__init__.py
+++ stacks/www/models/__init__.py
@@ -1,6 +1,6 @@
 from stacks.www.models.user import *
 from stacks.www.models.layout import *
-from stacks.www.models.library_item import *
+from stacks.www.models.item import *
 from stacks.www.models.site import *
 from stacks.www.models.stack import *
 from stacks.www.models.block import *
---
+++ stacks/www/models/item.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from stacks.www.models.utils import CacheMixin
+
+class Item(CacheMixin, models.Model):
+    user = models.ForeignKey(User, null=True, blank=True, related_name="library_items")
+    path = models.CharField(max_length=255, db_index=True)
+    value = models.TextField(null=True, blank=True)
+    added = models.DateTimeField(auto_now_add=True, db_index=True) # the date the item was added to the db
+    modified = models.DateTimeField(auto_now=True, db_index=True)
+
+    def __unicode__(self):
+        return self.full_path
+
+    class Meta:
+        app_label = 'www'
+        unique_together = (("user", "path"),)
+
+    @property
+    def full_path(self):
+        if self.user:
+            return 'library.users.%s.%s' % (self.user, self.path)
+        else:
+            return 'library.globals.%s' % self.path
