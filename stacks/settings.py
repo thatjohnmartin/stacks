866279ee26dd4f55fb00d3a6e5dd65fec80b81e9
John Martin <john@lonepixel.com>
2013-12-10 18:10:33 -0800

    Working on a merge.

--- stacks/settings.py
+++ stacks/settings.py
@@ -221,4 +221,4 @@ from django.conf import settings
 LOG_LEVEL = logging.DEBUG if settings.DEBUG else logging.INFO
 LOG_FORMAT = '%(asctime)s %(process)d %(filename)s(%(lineno)d): %(levelname)s %(message)s'
 
-logging.basicConfig(filename=settings.LOG_FILE, filemode='a', level=LOG_LEVEL, format=LOG_FORMAT)
+logging.basicConfig(filename=os.path.join(settings.LOG_ROOT, 'app.log'), filemode='a', level=LOG_LEVEL, format=LOG_FORMAT)
